b48f45bb473da635e51286707f2da112caafca0b
Shanly Suepaul <shanlys@gmail.com>
2019-04-01 15:39:56 -0400

    refactoring parsing functionality to use in multiple writers

---
+++ ibmCloudFunctions/lib/parseCatalogMessage.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const TOPIC_NAME = 'product-connect-jdbc-CATALOG';
+
+// Map of source attribute names to mapped name. Translatable attributes are suffixed with _EN, _ENG, or _FR.
+const translatableAttributeMap = {
+    'BRAND_NAME': 'brandName',
+    'DESC': 'name',
+    'MARKET_DESC': 'marketingDescription',
+    'DETAIL_DESC3': 'construction',
+    'FABRICANDMATERIAL': 'fabricAndMaterials',
+    'SIZE_DESC': 'styleAndMeasurements',
+    'CAREINSTRUCTIONS': 'careInstructions',
+    'ADVICE': 'advice',
+    'COLOUR_DESC': 'colour',
+    'COLOURGROUP': 'colourGroup'
+};
+// Map of source attribute names to mapped name. Non-translatable attribute names
+const attributeMap = {
+    'STYLEID': 'styleId',
+    'CATAGORY': 'level1Category',
+    'CATAGORY_LEVEL_1A': 'level2Category',
+    'CATAGORY_LEVEL_2A': 'level3Category',
+    'WEBSTATUS': 'webStatus',
+    'SEASON_CD': 'season',
+    'COLORID': 'colourId',
+    'APPROVED_FOR_WEB': 'approvedForWeb',
+};
+
+// Parse a message from the ELCAT.CATALOG table and return a new object with filtered and re-mapped attributes.
+export default function parseCatalogMessage(msg) {
+    if (msg.topic !== TOPIC_NAME) {
+        throw new Error('Can only parse Catalog update messages/');
+    }
+
+    // Re-map atttributes
+    const styleData = {};
+    for (let sourceAttributeName in translatableAttributeMap) {
+        styleData[translatableAttributeMap[sourceAttributeName]] = {
+            'en': msg.value[sourceAttributeName + '_EN'] || msg.value[sourceAttributeName + '_ENG'],
+            'fr': msg.value[sourceAttributeName + '_FR']
+        }
+    }
+    for (let sourceAttributeName in attributeMap) {
+        styleData[attributeMap[sourceAttributeName]] = msg.value[sourceAttributeName];
+    }
+
+    return styleData;
+}
