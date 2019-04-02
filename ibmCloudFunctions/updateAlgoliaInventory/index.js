0a202de4d2f6e6bd853ff57048063e7484faa812
Shanly Suepaul <shanlys@gmail.com>
2019-04-01 22:51:52 -0400

    adding algolia inventory writer

---
+++ ibmCloudFunctions/updateAlgoliaInventory/index.js
@@ -0,0 +1,45 @@
+const algoliasearch = require('algoliasearch');
+const parseSkuInventoryMessage = require('../lib/parseSkuInventoryMessage');
+
+let client = null;
+let index = null;
+
+async function main(params) {
+    if (!params.algoliaIndexName) {
+        throw new Error('Requires an Algolia index.');
+    }
+
+    if (!params.algoliaApiKey) {
+        throw new Error('Requires an API key for writing to Algolia.');
+    }
+
+    if (!params.algoliaAppId) {
+        throw new Error('Requires an App ID for writing to Algolia.');
+    }
+
+    if (!params.topicName) {
+        throw new Error('Requires an Event Streams topic.');
+    }
+
+    if (index === null) {
+        client = algoliasearch(params.algoliaAppId, params.algoliaApiKey);
+        index = client.initIndex(params.algoliaIndexName);
+    }
+
+    const updatedRecords = params.messages
+        .filter((msg) => msg.topic === params.topicName)
+        .map((msg) => parseSkuInventoryMessage(msg))
+        .map((inventory) => { return { ...inventory , objectID: inventory.styleId }; });
+
+    const promise = new Promise();
+    index.partialUpdateObjects(updatedRecords, true, (err) => {
+        if (err) {
+            throw new Error('Failed to update Algolia records: ' + err)
+        }
+
+        promise.resolve();
+    });
+    return promise;
+}
+
+exports.main = main;
