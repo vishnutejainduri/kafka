5d56171f6ef34c40eaed3981c10f8671b363d05f
Shanly Suepaul <shanlys@gmail.com>
2019-04-25 09:08:21 -0400

    Fixing webpack build

--- ibmCloudFunctions/consumeSkuInventoryMessage/webpack.config.js
+++ ibmCloudFunctions/consumeSkuInventoryMessage/webpack.config.js
@@ -4,10 +4,11 @@ module.exports = {
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        libraryTarget: 'umd'
     },
     target: 'node',
-    externals: {
-        'mongodb': 'commonjs mongodb'
-    }
+    externals: [
+        'mongodb'
+    ]
 };
