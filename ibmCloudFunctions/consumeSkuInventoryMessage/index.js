0e14d91fe9452f4507c17835f42367edb8cc1951
Shanly Suepaul <shanlys@gmail.com>
2019-06-07 01:40:14 -0400

    Refactoring sku inventory consumer to aggregate size data as well

--- ibmCloudFunctions/consumeSkuInventoryMessage/index.js
+++ ibmCloudFunctions/consumeSkuInventoryMessage/index.js
@@ -10,28 +10,51 @@ global.main = async function (params) {
         throw new Error("Invalid arguments. Must include 'messages' JSON array with 'value' field");
     }
 
-    const inventory = await getCollection(params);
+    const [inventory, styles, skus] = await Promise.all([
+        getCollection(params),
+        getCollection(params, params.stylesCollectionName),
+        getCollection(params, params.skusCollectionName)
+    ]);
     return Promise.all(params.messages
         .filter(filterSkuInventoryMessage)
         .map(parseSkuInventoryMessage)
-        .map((inventoryData) => inventory.findOne({ _id: inventoryData._id })
-            .then((existingDocument) => existingDocument
-                ? inventory.updateOne({ _id: inventoryData._id, lastModifiedDate: { $lt: inventoryData.lastModifiedDate } }, { $set: inventoryData })
-                : inventory.insertOne(inventoryData)
-            ).then(() => console.log("Updated/inserted document " + inventoryData._id))
-            .catch((err) => {
-                console.error('Problem with document ' + inventoryData._id);
-                console.error(err);
-                if (!(err instanceof Error)) {
-                    const e = new Error();
-                    e.originalError = err;
-                    e.attemptedDocument = inventoryData;
-                    return e;
-                }
+        .map((inventoryData) => {
+            const updateInventory = inventory.findOne({ _id: inventoryData._id })
+                    .then((existingDocument) => existingDocument
+                        ? inventory.updateOne({ _id: inventoryData._id, lastModifiedDate: { $lt: inventoryData.lastModifiedDate } }, { $set: inventoryData })
+                        : inventory.insertOne(inventoryData)
+                    );
 
-                err.attemptedDocument = inventoryData;
-                return err;
-            })
+            // sku ids can be null...
+            const skuLookup = inventoryData.skuId
+                ? skus.findOne({ _id: inventoryData.skuId })
+                : null;
+
+            return Promise.all([updateInventory, skuLookup])
+                .then(([updateInventoryResult, sku]) => {
+                    // Update style size count if we have a valid SKU and inventory was updated
+                    if (sku && (updateInventoryResult.modifiedCount || updateInventoryResult.insertedId)) {
+                        // If this inventory update added stock then ensure the size is tracked, otherwise remove the size
+                        const updateToProcess = inventoryData.quantityOnHandSellable
+                            ? { $addToSet: { sizes: sku.size } }
+                            : { $pull: { sizes: sku.size } };
+                        return styles.updateOne({ _id: inventoryData.styleId }, updateToProcess);
+                    }
+                })
+                .catch((err) => {
+                    console.error('Problem with document ' + inventoryData._id);
+                    console.error(err);
+                    if (!(err instanceof Error)) {
+                        const e = new Error();
+                        e.originalError = err;
+                        e.attemptedDocument = inventoryData;
+                        return e;
+                    }
+
+                    err.attemptedDocument = inventoryData;
+                    return err;
+                });
+            }
         )
     ).then((results) => {
         const errors = results.filter((res) => res instanceof Error);
