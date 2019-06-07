0dd12a427720fd84ac8ffd29433b8219b866a995
Shanly Suepaul <shanlys@gmail.com>
2019-06-07 02:31:37 -0400

    adding function for syncing availability to algolia

--- ibmCloudFunctions/updateAlgoliaInventory/index.js
+++ ibmCloudFunctions/updateAlgoliaInventory/index.js
@@ -1,24 +1,12 @@
 const algoliasearch = require('algoliasearch');
-const { filterSkuInventoryMessage, parseSkuInventoryMessage } = require('../lib/parseSkuInventoryMessage');
+const getCollection = require('../lib/getCollection');
 
 let client = null;
 let index = null;
 
-global.main = function (params) {
-    if (!params.algoliaIndexName) {
-        throw new Error('Requires an Algolia index.');
-    }
-
-    if (!params.algoliaApiKey) {
-        throw new Error('Requires an API key for writing to Algolia.');
-    }
-
-    if (!params.algoliaAppId) {
-        throw new Error('Requires an App ID for writing to Algolia.');
-    }
-
-    if (!params.topicName) {
-        throw new Error('Requires an Event Streams topic.');
+global.main = async function (params) {
+    if (!params.algoliaIndexName || !params.algoliaApiKey || !params.algoliaAppId) {
+        throw new Error('Requires Algolia configuration. See manifest.yml');
     }
 
     if (index === null) {
@@ -26,20 +14,22 @@ global.main = function (params) {
         index = client.initIndex(params.algoliaIndexName);
     }
 
-    const updatedRecords = params.messages
-        .filter(filterSkuInventoryMessage)
-        .map(parseSkuInventoryMessage)
-        .map((inventory) => { return { ...inventory , objectID: inventory.styleId }; });
+    const styleAvailabilityCheckQueue = await getCollection(params);
+    const styles = await getCollection(params, params.stylesCollectionName);
+    const stylesToCheck = await styleAvailabilityCheckQueue.find().limit(200).toArray();
+    const styleIds = stylesToCheck.map((style) => style.styleId);
 
-    return new Promise((resolve) => {
-        index.partialUpdateObjects(updatedRecords, true, (err) => {
-            if (err) {
-                throw new Error('Failed to update Algolia records: ' + err)
-            }
-
-            resolve();
-        });
-    });
+    Promise.all(stylesToCheck.map((style) => styles.findOne({ _id: style.styleId })
+        .then((styleData) => {
+            return {
+                isSellable: !!styleData.sizes.length,
+                sizes: styleData.sizes,
+                objectID: styleData._id
+            };
+        })
+    )).then((styleAvailabilitiesToBeSynced) => index.partialUpdateObjects(styleAvailabilitiesToBeSynced, true))
+        .then(() => styleAvailabilityCheckQueue.deleteMany({ _id: { $in: styleIds } }))
+        .then(() => console.log('Updated availability for styles ', styleIds));
 }
 
 module.exports = global.main;
