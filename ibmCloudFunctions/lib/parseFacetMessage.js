6c38388997f65654f7b2dabd7c16de6b085b3187
Shanly Suepaul <shanlys@gmail.com>
2019-06-10 11:58:51 -0400

    fixing facet value attribute name

--- ibmCloudFunctions/lib/parseFacetMessage.js
+++ ibmCloudFunctions/lib/parseFacetMessage.js
@@ -13,17 +13,16 @@ const facetMap = {
 // Parse a message from the ELCAT.CATALOG table and return a new object with filtered and re-mapped attributes.
 function parseFacetMessage(msg) {
     const facetName = facetMap[msg.value.CATEGORY];
-    const returnVal = {
+    return {
         _id: msg.value.STYLEID + facetName,
         id: msg.value.STYLEID + facetName,
         styleId: msg.value.STYLEID,
-        facetName
+        facetName,
+        facetValue: {
+            en: msg.value.DESC_ENG,
+            fr: msg.value.DESC_FR
+        }
     };
-    returnVal[facetName] = {
-        en: msg.value.DESC_ENG,
-        fr: msg.value.DESC_FR
-    };
-    return returnVal;
 }
 
 module.exports = {
---
+++ ibmCloudFunctions/updateAlgoliaFacets/index.js
@@ -0,0 +1,45 @@
+const algoliasearch = require('algoliasearch');
+const getCollection = require('../lib/getCollection');
+
+let client = null;
+let index = null;
+
+global.main = async function (params) {
+    if (!params.algoliaIndexName || !params.algoliaApiKey || !params.algoliaAppId) {
+        throw new Error('Requires Algolia configuration. See manifest.yml');
+    }
+
+    if (index === null) {
+        client = algoliasearch(params.algoliaAppId, params.algoliaApiKey);
+        index = client.initIndex(params.algoliaIndexName);
+    }
+
+    const styleAvailabilityCheckQueue = await getCollection(params);
+    const styles = await getCollection(params, params.stylesCollectionName);
+    const stylesToCheck = await styleAvailabilityCheckQueue.find().limit(200).toArray();
+    const styleIds = stylesToCheck.map((style) => style.styleId);
+
+    let styleAvailabilitiesToBeSynced = await Promise.all(stylesToCheck.map((style) => styles.findOne({ _id: style.styleId })
+        // for some reason we don't have style data in the DPM for certain styles referenced in inventory data
+        .then((styleData) => {
+            return !styleData || !styleData.sizes
+                ? null
+                : {
+                    isSellable: !!styleData.sizes.length,
+                    sizes: styleData.sizes,
+                    objectID: styleData._id
+                };
+        })
+    ));
+    styleAvailabilitiesToBeSynced = styleAvailabilitiesToBeSynced.filter((styleData) => styleData);
+    if (styleAvailabilitiesToBeSynced.length) {
+        return index.partialUpdateObjects(styleAvailabilitiesToBeSynced, true)
+            .then(() => styleAvailabilityCheckQueue.deleteMany({ _id: { $in: styleIds } }))
+            .then(() => console.log('Updated availability for styles ', styleIds));
+    } else {
+        console.log('No updates to process.');
+        return styleAvailabilityCheckQueue.deleteMany({ _id: { $in: styleIds } });
+    }
+}
+
+module.exports = global.main;
