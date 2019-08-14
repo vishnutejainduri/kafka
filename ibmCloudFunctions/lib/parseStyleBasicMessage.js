8ffdca36d85a6c9f976fa766f70fcb8e1ce4d5b3
Aleksandar <aleksandar.s@myplanet.com>
2019-08-14 15:29:51 -0400

    AS check only for valid brand ids

--- ibmCloudFunctions/lib/parseStyleBasicMessage.js
+++ ibmCloudFunctions/lib/parseStyleBasicMessage.js
@@ -3,9 +3,14 @@
 const TOPIC_NAME = 'styles-basic-connect-jdbc';
 
 function parseStyleBasicMessage(msg) {
+    const validBrandIds = ["1", "2", "3"];
     if (msg.topic !== TOPIC_NAME) {
         throw new Error('Can only parse Style Basic update messages');
     }
+    if (validBrandIds.indexOf(msg.value.BRAND_ID) <= -1) {
+        throw new Error('Invalid brand id in message');
+    }
+
 
     return {
         _id: msg.value.STYLE_ID,
