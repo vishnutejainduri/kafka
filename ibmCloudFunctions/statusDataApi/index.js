1f0c92d8d8b3e2d9b315afc8753c1523d31c2bb5
Aleksandar <aleksandar.s@myplanet.com>
2019-09-04 18:00:40 -0400

    HRC-389 fundamental data api status checker cloud function first draft

---
+++ ibmCloudFunctions/statusDataApi/index.js
@@ -0,0 +1,36 @@
+const { productApiRequest } = require('../lib/productApi');
+
+global.main = async function (params) {
+  params.productApiClientId = '';
+  params.productApiHost = '';
+
+  const response = {
+    statusCode: 200, 
+    headers: { 'Content-Type': 'application/json' },
+    body: { msg: 'Success' }
+  };
+
+  const apiStatusChecks = [];  
+  apiStatusChecks.push(productApiRequest(params, `/style`));
+  apiStatusChecks.push(productApiRequest(params, `/sku`));
+  apiStatusChecks.push(productApiRequest(params, `/inventory`));
+
+  return Promise.all(apiStatusChecks)
+    .then((results) => {
+      const invalidCount = results.filter((result) => result.data.length <= 0).length;
+      if (invalidCount === 0) {
+        return response;
+      } else {
+        response.statusCode = 500;
+        response.body.msg = 'Missing critical data'
+        return response;
+      }
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.body.msg = error;
+      return response;
+    })
+}
+
+module.exports = global.main;
