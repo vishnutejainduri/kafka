74feb92ab67534eeb55cc059c6bbb83b35bbe56c
Houman Kamali <houman.kamali@outlook.com>
2019-11-14 12:51:10 -0500

    HRC-867: update build.sh

--- ibmCloudFunctions/build.sh
+++ ibmCloudFunctions/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo ">>> Building Action packages:"
+cd ibmcloudfunctions/lib && npm install
 find alert-checks/* -maxdepth 0 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && npm install && npm run build" \;
 find product-consumers/* -maxdepth 0 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && npm install && npm run build" \;
 echo ">>> Built Action packages"
