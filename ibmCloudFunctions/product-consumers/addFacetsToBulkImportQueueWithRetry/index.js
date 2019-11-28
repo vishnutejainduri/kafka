de00788f1098d7cc51fb7a3d4c17fe91b53e8a23
Houman Kamali <houman.kamali@outlook.com>
2019-11-28 09:33:21 -0500

    HRC-941: add package name to action name in conductor action

--- ibmCloudFunctions/product-consumers/addFacetsToBulkImportQueueWithRetry/index.js
+++ ibmCloudFunctions/product-consumers/addFacetsToBulkImportQueueWithRetry/index.js
@@ -1,3 +1,3 @@
 const composer = require('openwhisk-composer')
  
-module.exports = composer.retry(3, 'add-facets-to-bulk-import-queue');
+module.exports = composer.retry(3, 'product-consumers/add-facets-to-bulk-import-queue');
