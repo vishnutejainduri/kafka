bd1b3505b856db3bdb09839a87a14b2c45d1786d
Aleksandar <aleksandar.s@myplanet.com>
2019-12-06 12:31:43 -0500

    HRC-1045 some new unit tests for the broken down calc sku ats and calc style ats functions

--- ibmCloudFunctions/product-consumers/bulkCalculateAvailableToSell/__tests__/bulkCalculateAvailableToSell.test.js
+++ ibmCloudFunctions/product-consumers/bulkCalculateAvailableToSell/__tests__/bulkCalculateAvailableToSell.test.js
@@ -1,27 +1,50 @@
 const bulkCalculateAvailableToSell = require('../');
+const getCollection = require('../../../lib/getCollection');
+const { calculateSkuAts, calculateStyleAts }  = require('../utils');
 
 jest.mock("mongodb");
+
+const params = {
+    mongoUri: 'mongo-uri',
+    dbName: 'db-name',
+    mongoCertificateBase64: 'mong-certificate',
+    collectionName: 'bulkAtsRecalculateQueue',
+    styleAvailabilityCheckQueue: 'styleAvailabilityCheckQueue',
+    stylesCollectionName: 'styles',
+    skusCollectionName: 'skus',
+    storesCollectionName: 'stores',
+    inventoryCollectionName: 'inventory'
+}
 
 describe('bulkCalculateAvailableToSell', () => {
     it('missing all parameters; should fail', async () => {
         await expect(bulkCalculateAvailableToSell({})).rejects.toThrow();
     });
     it('valid message', async () => {
-        const params = {
-            mongoUri: 'mongo-uri',
-            dbName: 'db-name',
-            mongoCertificateBase64: 'mong-certificate',
-            collectionName: 'bulkAtsRecalculateQueue',
-            styleAvailabilityCheckQueue: 'styleAvailabilityCheckQueue',
-            stylesCollectionName: 'styles',
-            skusCollectionName: 'skus',
-            storesCollectionName: 'stores',
-            inventoryCollectionName: 'inventory'
-        }
         let response = null;
-        await bulkCalculateAvailableToSell(params);
-        // returns nothing/undefined if successfully run
-        console.log('response',response);
-        expect(response).toEqual(null);
+        response = await bulkCalculateAvailableToSell(params);
+        expect(response).toEqual(undefined);
+    });
+    it('valid calculateStyleAts execution', async () => {
+        const styles = await getCollection(params, params.stylesCollectionName)
+        const skus = await getCollection(params, params.skusCollectionName)
+        const stores = await getCollection(params, params.storesCollectionName)
+        const inventory = await getCollection(params, params.inventoryCollectionName)
+        const styleToRecalcAts = 'styleId';
+
+        const { skuAtsOperations, styleAts, styleOnlineAts } = await calculateStyleAts(styleToRecalcAts, styles, skus, stores, inventory);
+        expect(styleAts).toEqual([]);
+        expect(styleOnlineAts).toEqual([]);
+        expect(skuAtsOperations[0]).toBeInstanceOf(Promise);
+    });
+    it('valid calculateSkuAts execution', async () => {
+        const stores = await getCollection(params, params.storesCollectionName)
+        const inventory = await getCollection(params, params.inventoryCollectionName)
+        const skuRecord = { _id: 'skuId' };
+        const styleData = { _id: 'styleId', departmentId: 'departmentId' };
+
+        const { skuAts, skuOnlineAts } = await calculateSkuAts(skuRecord, styleData, stores, inventory);
+        expect(skuAts).toEqual([]);
+        expect(skuOnlineAts).toEqual([]);
     });
 });
