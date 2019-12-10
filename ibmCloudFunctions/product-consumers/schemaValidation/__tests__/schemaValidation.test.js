ad22df4bf99db4f49d27a7da3a1a4e3ec6700f0d
Houman Kamali <houman.kamali@outlook.com>
2019-12-10 12:49:14 -0500

    HRC-1079: implement logging to kafka

--- ibmCloudFunctions/product-consumers/schemaValidation/__tests__/schemaValidation.test.js
+++ ibmCloudFunctions/product-consumers/schemaValidation/__tests__/schemaValidation.test.js
@@ -1,21 +1,22 @@
 const schemaValidator = require('../index.js');
 
 describe("schemaValidator", function() {
+    jest.setTimeout(60000);
     describe("validates addFacetsToBulkImportQueue", function() {
-        it("throws an error if params is not valid", function() {
+        it("throws an error if params is not valid", async function() {
             const invalidParams = {
                 cfName: "addFacetsToBulkImportQueue"
             };
-            expect(() => schemaValidator(invalidParams)).toThrow(Error);
+            await expect(schemaValidator(invalidParams)).rejects.toThrow(Error);
         }),
-        it("returns params as is if the params is valid", function() {
+        it("returns params as is if the params is valid", async function() {
             const validParams = {
                 cfName: "addFacetsToBulkImportQueue",
                 messages: []
             };
-            expect(schemaValidator(validParams)).toEqual(validParams);
+            expect(await schemaValidator(validParams)).toEqual(validParams);
         });
-        it("returns messages as is if the messages is valid", function() {
+        it("returns messages as is if the messages is valid", async function() {
             const paramsWithValidMessages = {
                 cfName: "addFacetsToBulkImportQueue",
                 messages: [{
@@ -26,9 +27,9 @@ describe("schemaValidator", function() {
                     },
                 }]
             };
-            expect(schemaValidator(paramsWithValidMessages)).toEqual(paramsWithValidMessages);
+            expect(await schemaValidator(paramsWithValidMessages)).toEqual(paramsWithValidMessages);
         });
-        it("returns any messages that fail validation as invalidMessages", function() {
+        it("returns any messages that fail validation as invalidMessages", async function() {
             const invalidMessage = {
                 value: {
                     STYLEID: 'styleId',
@@ -37,11 +38,12 @@ describe("schemaValidator", function() {
                     // DESC_ENG: 'eng-desc'
                 },
             };
+
             const paramsWithInvalidMessages = {
                 cfName: "addFacetsToBulkImportQueue",
                 messages: [invalidMessage]
             };
-            const returnedParams = schemaValidator(paramsWithInvalidMessages);
+            const returnedParams = await schemaValidator(paramsWithInvalidMessages);
             expect(returnedParams.invalidMessages[0].message).toEqual(invalidMessage);
             expect(returnedParams.invalidMessages[0].error.debugInfo.validationErrors.length).toBeGreaterThan(0);
         });
--- ibmCloudFunctions/product-consumers/schemaValidation/index.js
+++ ibmCloudFunctions/product-consumers/schemaValidation/index.js
@@ -1,5 +1,6 @@
 const Ajv = require('ajv');
 const ajv = new Ajv({ allErrors: true });
+const { Kafka } = require('kafkajs');
 
 const createError = require('../../lib/createError');
 const { log } = require('../utils');
@@ -12,8 +13,30 @@ const validators = {
     }
 };
 
-global.main = function(params) {
+let cachedProducer = null;
+
+async function getProducer({ brokers, username, password }){
+    if (!cachedProducer) {
+        const kafka = new Kafka({
+            clientId: 'schemaValidation',
+            brokers,
+            ssl: true,
+            sasl: {
+                mechanism: 'plain', // scram-sha-256 or scram-sha-512
+                username,
+                password
+            },
+        });
+        cachedProducer = kafka.producer();
+    }
+    await cachedProducer.connect();
+    return cachedProducer;
+}
+
+
+global.main = async function(params) {
     const validate = validators[params.cfName];
+
     validate.params(params);
     if (validate.params.errors) {
         throw createError.failedSchemaValidation(
@@ -21,6 +44,7 @@ global.main = function(params) {
             params.cfName
         );
     }
+
     let invalidMessages = [];
     const validMessages = params.messages.filter(message => {
         validate.message(message);
@@ -42,6 +66,20 @@ global.main = function(params) {
         invalidMessages.forEach(({ message, error }) => {
             log(`Invalid message ${message} with error: ${error}`);
         });
+
+        // Begin logging to kafka
+        const producer = await getProducer({
+            brokers: typeof params.kafkaBrokers === 'string' ? params.kafkaBrokers.split(",") : params.kafkaBrokers,
+            username: params.kafkaUsername,
+            password: params.kafkaPassword
+        });
+
+        await producer.send({
+            topic: params.kafkainvalidMessagesDlqTopicName,
+            messages: invalidMessages.map(message => ({ value: JSON.stringify(message) }))
+        });
+        // End login to kafka
+
         return {
             ...params,
             messages: validMessages,
