eef32037915cce8127c3b294b680c2bffbd65f99
Houman Kamali <houman.kamali@outlook.com>
2019-12-11 16:14:09 -0500

    HRC-1083: fix issues in namings

--- ibmCloudFunctions/product-consumers/deleteCreateAlgoliaStylesWithRetry/index.js
+++ ibmCloudFunctions/product-consumers/deleteCreateAlgoliaStylesWithRetry/index.js
@@ -1,3 +1,3 @@
 const composer = require('openwhisk-composer')
  
-module.exports = composer.retry(3, 'product-consumers/consume-threshold-message');
+module.exports = composer.retry(3, 'product-consumers/delete-create-algolia-styles');
