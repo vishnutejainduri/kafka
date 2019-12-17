9d6ca87b5dc4c43da1c68200352dbf4e66e94404
Aleksandar <aleksandar.s@myplanet.com>
2019-12-17 16:48:38 -0500

    HRC-1118 correct liniting error

--- ibmCloudFunctions/product-consumers/consumeSkuMessage/index.js
+++ ibmCloudFunctions/product-consumers/consumeSkuMessage/index.js
@@ -24,7 +24,7 @@ global.main = async function (params) {
     return Promise.all(params.messages
         .filter(filterSkuMessage)
         .map(parseSkuMessage)
-        .map((skuData) => skus.findOne({ _id: skuData._id })
+        .map(addErrorHandling((skuData) => skus.findOne({ _id: skuData._id })
             .then((existingDocument) => (existingDocument && existingDocument.lastModifiedDate)
                   ? skus.updateOne({ _id: skuData._id, lastModifiedDate: { $lt: skuData.lastModifiedDate } }, { $set: skuData })
                   : skus.updateOne({ _id: skuData._id }, { $set: skuData }, { upsert: true }) // fix race condition
