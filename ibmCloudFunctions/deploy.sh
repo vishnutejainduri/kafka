37ee23adac3873ae7f66af97f41cdc88cccff834
Aleksandar <aleksandar.s@myplanet.com>
2019-12-18 15:53:29 -0500

    HOTFIX onetime deploy

--- ibmCloudFunctions/deploy.sh
+++ ibmCloudFunctions/deploy.sh
@@ -20,6 +20,6 @@ ibmcloud fn rule list
 echo ">>> Deploying Actions Using WhiskDeploy…"
 ibmcloud fn deploy --project .  -d -v
 # We deploy twice because if run only once for some reason the triggers do not fire the CFs"
-echo ">>> Deploying Actions Using WhiskDeploy again…"
-ibmcloud fn deploy --project .  -d -v
+#echo ">>> Deploying Actions Using WhiskDeploy again…"
+#ibmcloud fn deploy --project .  -d -v
 echo ">>> Successfully Deployed Actions Using WhiskDeploy."
