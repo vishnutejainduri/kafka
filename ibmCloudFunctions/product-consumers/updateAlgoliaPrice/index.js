50c0a72837f599f19ee51a8f862aca8cda069938
Houman Kamali <houman.kamali@outlook.com>
2020-01-21 18:21:59 -0500

    HRC-1574: use standard log to output the complete result

--- ibmCloudFunctions/product-consumers/updateAlgoliaPrice/index.js
+++ ibmCloudFunctions/product-consumers/updateAlgoliaPrice/index.js
@@ -71,13 +71,13 @@ global.main = async function (params) {
                 return null;
             }
             // TODO remove logs after debugging
-            log("update: ", update);
-            log("styleData: ", styleData);
-            log("priceData: ", priceData);
+            console.log("update: ", update);
+            console.log("styleData: ", styleData);
+            console.log("priceData: ", priceData);
             if (priceData) {
-                log("update.onlineSalePrice: ", update.onlineSalePrice, "priceData.onlineSalePrice: ", priceData.onlineSalePrice);
-                log("update.inStoreSalePrice: ", update.inStoreSalePrice, "priceData.inStoreSalePrice: ", priceData.inStoreSalePrice);
-                log("(update.onlineSalePrice === priceData.onlineSalePrice && update.inStoreSalePrice === priceData.inStoreSalePrice)", (update.onlineSalePrice === priceData.onlineSalePrice && update.inStoreSalePrice === priceData.inStoreSalePrice))
+                console.log("update.onlineSalePrice: ", update.onlineSalePrice, "priceData.onlineSalePrice: ", priceData.onlineSalePrice);
+                console.log("update.inStoreSalePrice: ", update.inStoreSalePrice, "priceData.inStoreSalePrice: ", priceData.inStoreSalePrice);
+                console.log("(update.onlineSalePrice === priceData.onlineSalePrice && update.inStoreSalePrice === priceData.inStoreSalePrice)", (update.onlineSalePrice === priceData.onlineSalePrice && update.inStoreSalePrice === priceData.inStoreSalePrice))
             }
             const priceUpdate = generateUpdateFromParsedMessage (update, priceData, styleData);
             priceUpdate.objectID = styleData._id;
