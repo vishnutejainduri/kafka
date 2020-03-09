356555a62c51182a239d7f74855e107d61cd256a
Aleksandar <aleksandar.s@myplanet.com>
2020-03-09 13:26:27 -0400

    HRC-1770 unit tests for new stores CF

--- ibmCloudFunctions/product-consumers/consumeStoresMessage/__tests__/consumeStoresMessage.test.js
+++ ibmCloudFunctions/product-consumers/consumeStoresMessage/__tests__/consumeStoresMessage.test.js
@@ -1,44 +1,89 @@
 const consumeStoresMessage = require('../');
+const { getBulkAtsStyles } = require('../utils');
+const { parseStoreMessage } = require('../../../lib/parseStoreMessage');
 
-jest.mock("mongodb");
+const getCollection = require('../../../lib/getCollection');
+
+const params = {
+    topicName: 'stores-connect-jdbc',
+    messages: [{
+        topic: 'stores-connect-jdbc',
+        value: {
+            'SITE_ID': '00007',
+            'NAME': 'name',
+            'ADDRESS1': 'address1',
+            'ADDRESS2': 'address2',
+            'CITY': 'city',
+            'STATE': 'state',
+            'POSTALZIP': 'zipCode',
+            'PHONE': 'telephone',
+            'LATITUDE': 43.6535366,
+            'LONGITUDE': -79.3799731,
+            'FULFILL_STATUS': 'Y',
+            'PICKUP_STATUS': 'Y',
+            'STORELOOKUP_STATUS': 'Y',
+            'DEP27_FULFILL_STATUS': 'Y',
+            'DAYS_OPEN_PER_WEEK': 7,
+            'ZONE_ID': 1,
+            'DATE_CLOSED': null,
+            'POS_ENABLED': 'Y',
+            'SUB_TYPE': 'subType',
+            'LASTMODIFIEDDATE': 1000000000,
+          }
+    }],
+    mongoUri: 'mongo-uri',
+    dbName: 'db-name',
+    mongoCertificateBase64: 'mong-certificate',
+    collectionName: 'stores',
+    inventoryCollectionName: 'inventory',
+    bulkAtsRecalculateQueue: 'bulkAtsRecalculateQueue'
+};
 
 describe('consumeStoresMessage', () => {
     it('missing all parameters; should fail', async () => {
         await expect(consumeStoresMessage({})).rejects.toThrow();
     });
-    it('correct message', async () => {
-        const params = {
-            topicName: 'stores-connect-jdbc',
-            messages: [{
-                topic: 'stores-connect-jdbc',
-                value: {
-                    'BUSINESS_UNIT_ID': 1,
-                    'SUB_TYPE': 'subType',
-                    'SITE_ID': 'siteId',
-                    'ZONE_ID': 'zoneId',
-                    'DAYS_OPEN_PER_WEEK': 7,
-                    'NAME': 'name',
-                    'ADDRESS_1': 'address1',
-                    'ADDRESS_2': 'address2',
-                    'ADDRESS_3': 'address3',
-                    'ADDRESS_4': 'address4',
-                    'CITY': 'city',
-                    'STATE_ID': 'stateId',
-                    'ZIP_CODE': 'zipCode',
-                    'COUNTRY_ID': 'countryId',
-                    'TELEPHONE': 'telephone',
-                    'FAX': 'fax',
-                    'LATITUDE': 43.6535366,
-                    'LONGITUDE': -79.3799731
-                  }
-            }],
-            mongoUri: 'mongo-uri',
-            dbName: 'db-name',
-            mongoCertificateBase64: 'mong-certificate',
-            collectionName: 'stores'
-        }
-        //const response = await consumeStoresMessage(params);
+    it('correct message; no bulk ats', async () => {
+        const response = await consumeStoresMessage(params);
         // returns nothing/undefined if successfully run
-        expect(params).toBeTruthy();
+        expect(response).toBe(undefined);
+    });
+    it('correct message; goes through bulk ats code', async () => {
+        params.POS_ENABLED = 'N';
+
+        const response = await consumeStoresMessage(params);
+        // returns nothing/undefined if successfully run
+        expect(response).toBe(undefined);
+    });
+});
+
+describe('getBulkAtsStyles', () => {
+    it('missing all parameters; should fail', async () => {
+        await expect(getBulkAtsStyles()).rejects.toThrow();
+    });
+    it('correct message; should return a style', async () => {
+        const inventory = await getCollection(params, params.inventoryCollectionName);
+        const bulkAtsRecalculateQueue = await getCollection(params, params.bulkAtsRecalculateQueue);
+        let bulkStyleAtsUpdates = bulkAtsRecalculateQueue.initializeUnorderedBulkOp();
+
+        bulkStyleAtsUpdates = await getBulkAtsStyles(bulkStyleAtsUpdates, { _id: '00007' }, inventory);
+
+        const response = await bulkStyleAtsUpdates.find().upsert().updateOne();
+
+        // returns nothing/undefined if successfully run
+        expect(response._id).toBe('success');
+    });
+});
+
+describe('parseStoreMessage', () => {
+    it('wrong topic; should fail', () => {
+        const wrongTopicMessage = { topic: 'foo' };
+        expect(parseStoreMessage.bind(null, wrongTopicMessage)).toThrow('Can only parse store update messages');
+    });
+    it('correct message; should return transformed store obj', async () => {
+        const response = parseStoreMessage(params.messages[0]);
+
+        // check one of the most critical fields, if true, transform must have worked well
+        expect(response.isVisible).toBe(true);
     });
 });
