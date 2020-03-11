db35789d3c32f7e2f584d7493b751210ea371d57
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-03-11 09:03:59 -0400

    Move helper functions to own file

---
+++ ibmCloudFunctions/product-consumers/consumeCatalogMessageCT/APIHelpers.js
@@ -0,0 +1,88 @@
+const fetch = require('node-fetch');
+
+const CT_ENDPOINT = 'https://api.us-central1.gcp.commercetools.com/harryrosen-dev'; // TODO: move to constants or ENV file
+const BEARER_TOKEN = process.env.BEARER_TOKEN; // TODO: switch to using secret to fetch fresh bearer token
+
+const handleAPIError = err => {
+  console.error('TODO: add proper error handling');
+  console.error(err);
+};
+
+const Authorization = `Bearer ${BEARER_TOKEN}`;
+
+const getStyleVersion = async styleId => {
+  const response = await fetch(`${CT_ENDPOINT}/products/${styleId}`, { headers: { Authorization }}); // TODO: change to key (which can be custom, to correspond to the style IDå--the ID is just whatever CT spits out)
+  if (response.status === 404) return null; // indicates that style doesn't exist in CT
+  const style = await response.json();
+  return style.version;
+};
+
+// Returns true iff the given attribute is a custom attribute on the HR product
+// type defined in CT
+const isCustomAttribute = attribute => {
+  const customAttributes = [
+    'season',
+    'brandName',
+    'construction',
+    'fabricAndMaterials',
+    'styleAndMeasurements',
+    'careInstructions',
+    'advice',
+    'webStatus',
+    'vsn'
+  ];
+
+  return customAttributes.includes(attribute);
+};
+
+// Returns an array of actions, each of which tells CT to update a different
+// attribute of the given style
+const getActionsFromStyle = style => {
+  const customAttributesToUpdate = Object.keys(style).filter(isCustomAttribute);
+
+  const customAttributeUpdateActions = customAttributesToUpdate.map(attribute => ({
+    action: "setAttributeInAllVariants",
+    name: attribute,
+    value: style[attribute]
+  }));
+
+  // `name` isn't a custom attribute of products in CT, so its update action looks different from the others
+  const nameUpdateAction = style.name ? { action: "changeName", name: style.name } : null; // TODO: make sure localized strings are formatted correctly (e.g., `en-CA` keys instead if just `en`)
+
+  const allUpdateActions = nameUpdateAction
+    ? [...customAttributeUpdateActions, nameUpdateAction]
+    : customAttributeUpdateActions;
+
+  return allUpdateActions;
+};
+
+const updateStyle = (style, version) => {
+  const actions = getActionsFromStyle(style);
+  const body = JSON.stringify({ version, actions }); // stringify?
+  const headers = { Authorization };
+  const method = 'post';
+
+  return fetch(`${CT_ENDPOINT}/products/${style.id}`, { method, headers, body }); // TODO: change to ?key=...
+};
+
+const createStyle = style => {
+  // TODO: remember to set a key to the value of style.id
+};
+
+const createOrUpdateStyle = async style => {
+  try {
+    const currentProductVersion = await getStyleVersion(style.id);
+    if (!currentProductVersion) { // the style isn't currently stored in CT, so we create a new one
+      return createStyle(style);
+    } else { // the style is already stored in CT, so we just need to update its attributes
+      return updateStyle(style, currentProductVersion);
+    }
+  } catch (err) {
+    handleAPIError(err);
+  }
+};
+
+module.exports = {
+  createOrUpdateStyle,
+  handleAPIError
+};
