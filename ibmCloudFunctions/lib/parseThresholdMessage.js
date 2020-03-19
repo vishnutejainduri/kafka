f72c0160166ec22f4f916eec3d37e3a8434f2789
Aleksandar <aleksandar.s@myplanet.com>
2020-03-19 16:31:31 -0400

    HRC-1958-EMERGENCY everything from the regular branch named HRC-1958

--- ibmCloudFunctions/lib/parseThresholdMessage.js
+++ ibmCloudFunctions/lib/parseThresholdMessage.js
@@ -2,6 +2,27 @@
 
 const TOPIC_NAME = 'thresholds-connect-jdbc';
 
+// some js "reverse engineered" from some scala code that can be found in the thread here:
+// https://github.com/confluentinc/kafka-connect-jdbc/issues/563
+function convertToInteger(inputString) {
+  const buffer = Buffer.from(inputString, 'base64'); //makes a buffer from the base64 string input
+  const binaryString = buffer.toString('binary'); //convert buffer to binary string...
+
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i); //gets unicode value of current decoded string character
+  }
+
+  let finalIntegerValue = 0;
+  for (let i = 0; i < bytes.length; i++) {
+    finalIntegerValue = (finalIntegerValue << 8) | bytes[i]; //bitwise operations... turn to integer with things
+  }
+
+  return finalIntegerValue;
+}
+
 function parseThresholdMessage(msg) {
     if (msg.topic !== TOPIC_NAME) {
         throw new Error('Can only parse Threshold update messages');
@@ -9,7 +30,7 @@ function parseThresholdMessage(msg) {
 
     return {
         skuId: msg.value.SKU_ID,
-        threshold: msg.value.THRESHOLD
+        threshold: convertToInteger(msg.value.THRESHOLD)
     };
 }
 
