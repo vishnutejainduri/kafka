4ebe671c080801b3b91f85ae295dc250d4dbad85
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-03-20 11:45:14 -0400

    Implement basic tests

---
+++ ibmCloudFunctions/commercetools/consumeSkuMessageCT/__tests__/index.test.js
@@ -0,0 +1,46 @@
+const consumeSkuMessageCT = require('..');
+
+jest.mock('@commercetools/sdk-client');
+jest.mock('@commercetools/api-request-builder');
+jest.mock('@commercetools/sdk-middleware-auth');
+jest.mock('@commercetools/sdk-middleware-http');
+jest.mock('node-fetch');
+
+const validParams = {
+  topicName: 'skus-connect-jdbc',
+  messages: [{
+      topic: 'skus-connect-jdbc',
+      value: {
+        ID:'skuId',
+        STYLEID:'styleId',
+        COLORID:'colorId',
+        SIZEID: 'sizeId',
+        SIZE: 'size',
+        DIMENSION: 'dimension',
+        LASTMODIFIEDDATE:1000000000,
+        FKORGANIZATIONNO: '1'
+      }
+  }],
+  mongoUri: 'mongo-uri',
+  dbName: 'db-name',
+  mongoCertificateBase64: 'mong-certificate',
+  collectionName: 'skus',
+  ctpProjectKey: 'key',
+  ctpClientId: 'id',
+  ctpClientSecret: 'secret',
+  ctpAuthUrl: 'authUrl',
+  ctpApiUrl: 'apiUrl',
+  ctpScopes: 'manage_products:harryrosen-dev'
+};
+
+describe('consumeSkuMessageCT', () => {
+  it('throws an error if given params are invalid', () => {
+    const invalidParams = {};
+    return expect(consumeSkuMessageCT(invalidParams)).rejects.toThrow();
+  });
+
+  it('returns  `undefined` if given valid params', async () => {
+    const response = await consumeSkuMessageCT(validParams);
+    expect(response).toBeUndefined();
+  });
+});
