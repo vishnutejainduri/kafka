44a256aa3fb2535691568e673da8b1d97eabfe23
Houman Kamali <houman.kamali@outlook.com>
2020-03-23 11:59:14 -0400

    HRC-2000: update retries to backoff exponentially

--- ibmCloudFunctions/product-consumers/resolveMessagesLogs/utils.js
+++ ibmCloudFunctions/product-consumers/resolveMessagesLogs/utils.js
@@ -1,5 +1,5 @@
-const MAX_RETRIES = 24;
-const INTERVAL_PER_RETRY = 10 * 60 * 1000;
+const MAX_RETRIES = 6;
+const INTERVAL_PER_RETRY = 15 * 60 * 1000;
 
 function getValueWithUpdatedMetadata(value, activationEndTime) {
     const metadata = value.metadata || {};
@@ -10,7 +10,7 @@ function getValueWithUpdatedMetadata(value, activationEndTime) {
                 ...metadata,
                 retries,
                 lastRetry: activationEndTime,
-                nextRetry: activationEndTime + INTERVAL_PER_RETRY * retries
+                nextRetry: activationEndTime + INTERVAL_PER_RETRY * Math.pow(2, retries)
                 // Current implementation requires a process job that is invoked regularly in a high frequency e.g. every minute and finds all the messages
                 // that have had a next retry before the process job run.
                 // Alternative implementation: next retry could instead be a bucket e.g. immediately for retries 0 to 2 and next hours for retries 2 to 4, etc.
