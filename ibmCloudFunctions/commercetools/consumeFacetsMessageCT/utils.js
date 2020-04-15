d121a7d1bab78954faa8423acafb1f181726ed25
Aleksandar <aleksandar.s@myplanet.com>
2020-04-15 17:35:54 -0400

    HRC-2204 linting cleanup

--- ibmCloudFunctions/commercetools/consumeFacetsMessageCT/utils.js
+++ ibmCloudFunctions/commercetools/consumeFacetsMessageCT/utils.js
@@ -2,7 +2,7 @@ const { getExistingCtStyle, getProductType, updateStyle, createAndPublishStyle }
 
 const updateStyleFacets = async (ctHelpers, productTypeId, stylesFacetMessage) => {
     const productType = await getProductType(productTypeId, ctHelpers);
-    const existingCtStyle = await getExistingCtStyle(stylesFacetMessage.id, ctHelpers);
+    let existingCtStyle = await getExistingCtStyle(stylesFacetMessage.id, ctHelpers);
 
     if (!existingCtStyle) {
       existingCtStyle = (await createAndPublishStyle ({ id: stylesFacetMessage.id, name: { 'en-CA': '', 'fr-CA': '' } }, { id: productTypeId }, null, ctHelpers)).body;
--- ibmCloudFunctions/commercetools/consumeStylesBasicMessageCT/utils.js
+++ ibmCloudFunctions/commercetools/consumeStylesBasicMessageCT/utils.js
@@ -3,10 +3,10 @@ const { styleAttributeNames } = require('../constantsCt');
 
 const updateStyleOutlet = async (ctHelpers, productTypeId, stylesBasicMessage) => {
     const productType = await getProductType(productTypeId, ctHelpers);
-    const existingCtStyle = await getExistingCtStyle(stylesBasicMessage.id, ctHelpers);
+    let existingCtStyle = await getExistingCtStyle(stylesBasicMessage.id, ctHelpers);
 
     if (!existingCtStyle) {
-      existingCtStyle = (await createAndPublishStyle ({ id: stylesFacetMessage.id, name: { 'en-CA': '', 'fr-CA': '' } }, { id: productTypeId }, null, ctHelpers)).body;
+      existingCtStyle = (await createAndPublishStyle ({ id: stylesBasicMessage.id, name: { 'en-CA': '', 'fr-CA': '' } }, { id: productTypeId }, null, ctHelpers)).body;
     }
     if (existingCtStyleIsNewer(existingCtStyle, stylesBasicMessage, styleAttributeNames.STYLE_OUTLET_LAST_MODIFIED_INTERNAL)) {
       return null;
