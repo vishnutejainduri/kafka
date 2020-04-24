ccbf61fd53428f3d30d4c35a76618aef7051ccd5
Aleksandar <aleksandar.s@myplanet.com>
2020-04-24 11:55:15 -0400

    HRC-2289 delete all orders and carts

--- tools/commercetools/bulkDelete/resourceDeleter.sh
+++ tools/commercetools/bulkDelete/resourceDeleter.sh
@@ -1,5 +1,5 @@
-PROJECT_KEY = <PROJECT_KEY>
-ACCESS_TOKEN = <ACCESS_TOKEN>
+PROJECT_KEY=<PROJECT_KEY>
+ACCESS_TOKEN=<ACCESS_TOKEN>
 
 # delete products (including variants)
 resource-deleter -p $PROJECT_KEY -r products --api-url "https://api.us-central1.gcp.commercetools.com" --authUrl "https://auth.us-central1.gcp.commercetools.com" --accessToken $ACCESS_TOKEN
@@ -9,3 +9,9 @@ resource-deleter -p $PROJECT_KEY -r customObjects --api-url "https://api.us-cent
 
 # delete categories
 resource-deleter -p $PROJECT_KEY -r categories --api-url "https://api.us-central1.gcp.commercetools.com" --authUrl "https://auth.us-central1.gcp.commercetools.com" --accessToken $ACCESS_TOKEN
+
+# delete carts
+resource-deleter -p $PROJECT_KEY -r carts --api-url "https://api.us-central1.gcp.commercetools.com" --authUrl "https://auth.us-central1.gcp.commercetools.com" --accessToken $ACCESS_TOKEN
+
+# delete orders
+resource-deleter -p $PROJECT_KEY -r orders --api-url "https://api.us-central1.gcp.commercetools.com" --authUrl "https://auth.us-central1.gcp.commercetools.com" --accessToken $ACCESS_TOKEN
