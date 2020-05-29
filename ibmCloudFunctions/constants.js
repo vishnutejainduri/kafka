186d2566b7921efb30dfdb83f3161487083acff1
Houman Kamali <houman.kamali@outlook.com>
2020-05-29 12:22:29 -0400

    HRC-2453: add more comments

--- ibmCloudFunctions/constants.js
+++ ibmCloudFunctions/constants.js
@@ -10,6 +10,7 @@ module.exports = {
   // For commercetools, we want to process only prices set for the online store i.e. those with SITE_ID === '00990'
   // For mongo/algolia we need to distinguish between these prices, because they will be used by advisor app who need access to both
   siteIds: {
+    // This refers to the flagship store at Bloor whish should have all of the items
     IN_STORE: '00011',
     ONLINE: '00990'
   }
