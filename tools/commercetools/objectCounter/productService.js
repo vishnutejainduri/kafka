48a4d7c82fef7b870bc8103aa30ff6ed2f64f557
Aleksandar <aleksandar.s@myplanet.com>
2020-07-02 14:48:52 -0400

    HRC-2788 code to get all variants from all products

--- tools/commercetools/objectCounter/productService.js
+++ tools/commercetools/objectCounter/productService.js
@@ -1,20 +1,36 @@
 const getAllVariantsCount = async ({ client, requestBuilder }) => {
   const method = 'GET';
 
-  const uri = requestBuilder.products.build();
+  let variantTotal = 0;
+  let productTotal = 0;
+  let lastId = null;
+  let resultCount = 500;
+  
+  while (resultCount === 500) {
+    let uri;
+    if (!lastId) {
+      uri = requestBuilder.products.withTotal(false).perPage(500).sort('id').build();
+    } else {
+      uri = requestBuilder.products.withTotal(false).perPage(500).sort('id').where(`id > "${lastId}"`).build();
+    }
 
-  try {
-    const response = await client.execute({ method, uri });
-    const results = response.body.results;
-    console.log('Total products: ', results.length);
+    try {
+      const response = await client.execute({ method, uri });
 
-    let variantTotal = 0;
-    results.forEach ((result) => variantTotal += result.masterData.current.variants.length )
-    return variantTotal;
-  } catch (err) {
-      if (err.code === 404) return null;
-      throw err;
+      resultCount = response.body.count;
+      productTotal += resultCount;
+      console.log('Total products: ', productTotal);
+
+      const results = response.body.results;
+      results.forEach ((result) => variantTotal += result.masterData.current.variants.length )
+      console.log('Total variants: ', variantTotal);
+      lastId = results[results.length-1].id;
+    } catch (err) {
+        if (err.code === 404) return null;
+        throw err;
+    }
   }
+  return variantTotal;
 }
 
 module.exports.getAllVariantsCount = getAllVariantsCount;
