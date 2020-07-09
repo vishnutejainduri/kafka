afd50c657c21a1ebc93de32d75aac6ef140f352b
Houman Kamali <houmam.kamali@outlook.com>
2020-07-09 13:02:01 -0400

    HRC-2776: add another test case

--- ibmCloudFunctions/lib/__tests__/utils.js
+++ ibmCloudFunctions/lib/__tests__/utils.js
@@ -8,12 +8,18 @@ describe('groupByAttribute', function () {
     firstBatch.originalIndexes = [2, 3]
     secondBatch.originalIndexes = [4]
 
-    it('handles errors and nulls', function () {
+    it('handles errors and nulls and valid items', function () {
         const result = groupByAttribute('styleId')(items)
         expect(result).toEqual([
             firstBatch,
             secondBatch
         ])
+    })
+
+    it('handles errors and nulls', function () {
+        const items = [null, new Error('')]
+        const result = groupByAttribute('styleId')(items)
+        expect(result).toEqual([])
     })
 
     it('throws an error if the attribute does not exist in valid items', function () {
