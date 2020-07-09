bc3df8c3ca24bf42c189ab9b23a040c7d2e3a0d2
Aleksandar <aleksandar.s@myplanet.com>
2020-07-09 14:12:37 -0400

    HRC-2834 final code for counting barcodes

--- tools/commercetools/objectCounter/productService.js
+++ tools/commercetools/objectCounter/productService.js
@@ -1,5 +1,10 @@
 const countImage = (result) => result.masterData.current.variants.reduce((totalImages, currentVariant) => (totalImages += currentVariant.images.length), 0 )
-const countBarcodes = (result) => result.masterData.current.variants.reduce((totalBarcodes, currentVariant) => (totalBarcodes += currentVariant.barcodes ? currentVariant.attributes.barcodes.value.length : 0), 0 )
+const countBarcodes = (result) => {
+  return result.masterData.current.variants.reduce((totalBarcodes, currentVariant) => {
+    const barcodeAttribute = currentVariant.attributes.filter((attribute) => attribute.name === 'barcodes')[0];
+    return totalBarcodes += barcodeAttribute ? barcodeAttribute.value.length : 0
+  }, 0)
+}
 const countVariants = (result) => result.masterData.current.variants.length
 
 const getAllVariantAttributeCount = async ({ client, requestBuilder }, counterFunction) => {
