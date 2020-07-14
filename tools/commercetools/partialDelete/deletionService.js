f2e897605434a4427994825465cef3aaf39e939b
Aleksandar <aleksandar.s@myplanet.com>
2020-07-14 16:08:02 -0400

    HRC-2860 partial delete script finish

--- tools/commercetools/partialDelete/deletionService.js
+++ tools/commercetools/partialDelete/deletionService.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const { createSyncProducts } = require('@commercetools/sync-actions');
 const syncProducts = createSyncProducts()
 
+let totalFailures = 0;
+let totalSuccess = 0;
+
 const wstreamError = fs.createWriteStream('errors.csv');
 
 const deletePrices = (product) => {
@@ -15,15 +18,40 @@ const deletePrices = (product) => {
 
 const whereHasPrice = `masterData(current(masterVariant(prices is not empty))) or masterData(current(variants(prices is not empty)))`
 
-const deleteProductData = async ({ client, requestBuilder }, { priceDeleter }, { whereQuery }) => {
+const sendUpdateActions = ({ client, requestBuilder }, actions, productBefore) => {
+  if (actions.length === 0) return null;
+  const uri = requestBuilder.products.byKey(productBefore.key).build();
+  const body = JSON.stringify({ version: productBefore.version, actions });
+
+  return client.execute({ method: 'POST', uri, body })
+    .then(result => {
+      const uri = requestBuilder.products.byKey(result.body.key).build();
+      const body = JSON.stringify({ version: result.body.version, actions: [{ action: 'publish', scope: 'All' }] });
+
+      return client.execute({ method: 'POST', uri, body })
+        .then(publishResult => totalSuccess += 1)
+        .catch(error => {
+          console.error(error.message);
+          wstreamError.write(result.body.key + ',' + error.message + '\n');
+          totalFailures += 1;
+        })
+    })
+    .catch(error => {
+      console.error(error.message);
+      wstreamError.write(productBefore.key + ',' + error.message + '\n');
+      totalFailures += 1;
+    })
+}
+
+const deleteProductData = async (ctHelpers, { priceDeleter }, whereQuery) => {
   const method = 'GET';
 
   let productTotal = 0;
-  let totalFailures = 0;
-  let totalSuccess = 0;
 
   let lastId = null;
   let resultCount = 500;
+
+  const { client, requestBuilder } = ctHelpers;
   
   while (resultCount === 500) {
     let uri;
@@ -42,44 +70,35 @@ const deleteProductData = async ({ client, requestBuilder }, { priceDeleter }, {
       console.log('Total products: ', productTotal);
       const results = response.body.results;
 
-      await Promise.all(results.map ((result) => {
+      let syncActions = results.map ((result) => {
         const productBefore = result;
         let productAfter = JSON.parse(JSON.stringify(productBefore));
 
         if (priceDeleter) productAfter = priceDeleter(productAfter);
-        //console.log('productAfter', productAfter.masterData.current)
-        //console.log('productBefore', productBefore.masterData.current)
 
-        let actions = syncProducts.buildActions(productAfter.masterData.current, productBefore.masterData.current);
-        actions = actions.map(action => {
-          action['isStaged'] = false
-          return action;
+        const actions = syncProducts.buildActions(productAfter.masterData.current, productBefore.masterData.current);
+        const groupedActionPromises = [];
+        let groupedActions = [];
+        actions.forEach((action, index) => {
+          groupedActions.push(action);
+          if ((index+1) % 500 === 0) { //send every 500 actions
+            groupedActionPromises.push(sendUpdateActions(ctHelpers, groupedActions, productBefore));
+            groupedActions = [];
+          } else if (index === actions.length-1) { //end of actions, send remainder
+            groupedActionPromises.push(sendUpdateActions(ctHelpers, groupedActions, productBefore));
+            groupedActions = [];
+          }
         });
-        //console.log('actions', actions);
-        const uri = requestBuilder.products.byKey(productBefore.key).build();
-        //console.log('productBefore.key', productBefore.key);
-        const body = JSON.stringify({ version: productBefore.version, actions });
+        return groupedActionPromises;
+      });
 
-        return client.execute({ method: 'POST', uri, body })
-          .then(result => {
-            //console.log('result', result.body.version, 'productBefore.version', productBefore.version);
-            const uri = requestBuilder.products.byKey(result.body.key).build();
-            const body = JSON.stringify({ version: result.body.version, actions: [{ action: 'publish', scope: 'All' }] });
-
-            return client.execute({ method: 'POST', uri, body })
-              .then(publishResult => totalSuccess += 1)
-              .catch(error => {
-                console.error(error.message);
-                wstreamError.write(result.body.key + ',' + error.message + '\n');
-                totalFailures += 1;
-              })
-          })
-          .catch(error => {
-            console.error(error.message);
-            wstreamError.write(productBefore.key + ',' + error.message + '\n');
-            totalFailures += 1;
-          })
-      }));
+      syncActions = syncActions.reduce((finalActions, currentActions) => [...finalActions, ...currentActions], []).filter(action => action);
+      await Promise.all (syncActions)
+        .catch(error => {
+          console.error(error);
+          wstreamError.write(results + ',' + error + '\n');
+          totalFailures += syncActions.length;
+        })
 
       lastId = results[results.length-1].id;
     } catch (err) {
@@ -94,7 +113,7 @@ const deleteProductData = async ({ client, requestBuilder }, { priceDeleter }, {
   };
 }
 
-const deleteAllPrices = (ctHelpers) => deleteProductData(ctHelpers, { priceDeleter: deletePrices }, { whereQuery: whereHasPrice })
+const deleteAllPrices = (ctHelpers) => deleteProductData(ctHelpers, { priceDeleter: deletePrices }, whereHasPrice)
 
 module.exports = {
   deleteAllPrices
