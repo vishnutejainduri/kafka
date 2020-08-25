5604cb7fcaaf0971a81a2df57136b46b658d15e7
Aleksandar <aleksandar.s@myplanet.com>
2020-08-25 16:49:45 -0400

    HRC-3294 add unit test to updateAlgoliaInventory

--- ibmCloudFunctions/product-consumers/updateAlgoliaInventory/__tests__/utils.test.js
+++ ibmCloudFunctions/product-consumers/updateAlgoliaInventory/__tests__/utils.test.js
@@ -1,5 +1,167 @@
-describe('updateAlgoliaInventory utils', () => {
-    it('reads ATS from SKU collection', async () => {
-        // TODO implement integration test https://myplanet.jira.com/browse/HRC-1787
+const {
+  buildSizesArray,
+  buildStoreInventory,
+  buildStoresArray
+} = require('../utils.js');
+
+const validSku = {
+  _id: 'skuId',
+  size: {
+    en: 'sizeEn',
+    fr: 'sizeFr'
+  },
+  ats: [{
+    storeId: 'storeId',
+    availableToSell: 1
+  }],
+  onlineAts: [{
+    storeId: 'storeId',
+    availableToSell: 1
+  }]
+}
+
+const validSkuOutOfStock = {
+  _id: 'skuIdOutOfStock',
+  size: {
+    en: 'sizeEnOutOfStock',
+    fr: 'sizeFrOutOfStock'
+  },
+  ats: [],
+  onlineAts: []
+}
+
+const validSkuOutOfStockOnline = {
+  _id: 'skuIdOutOfStockOnline',
+  size: {
+    en: 'sizeEnOutOfStockOnline',
+    fr: 'sizeFrOutOfStockOnline'
+  },
+  ats: [{
+    storeId: 'storeIdOutOfStockOnline',
+    availableToSell: 1
+  }],
+  onlineAts: []
+}
+
+describe('buildSizesArray', () => {
+    it('two available skus should produce two available sizes', async () => {
+      const validSkus = [validSku, { ...validSku, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' } }]
+      const result = buildSizesArray(validSkus);
+      expect(result).toEqual([ { en: 'sizeEn', fr: 'sizeFr' }, { en: 'sizeEn2', fr: 'sizeFr2' } ]); 
+    });
+    it('two available skus with duplicate sizes should produce one available size', async () => {
+      const validSkus = [validSku, { ...validSku, _id: 'skuId2' }]
+      const result = buildSizesArray(validSkus);
+      expect(result).toEqual([ { en: 'sizeEn', fr: 'sizeFr' } ]); 
+    });
+    it('one available sku out of two skus should produce one available size', async () => {
+      const validSkus = [validSku, validSkuOutOfStock]
+      const result = buildSizesArray(validSkus);
+      expect(result).toEqual([ { en: 'sizeEn', fr: 'sizeFr' } ]); 
+    });
+    it('no available skus out of two skus should produce no available sizes', async () => {
+      const validSkus = [validSkuOutOfStock, { ...validSkuOutOfStock, _id: 'skuId2' } ]
+      const result = buildSizesArray(validSkus);
+      expect(result).toEqual([]); 
+    });
+    it('one online unavailable sku and one instore available sku should produce one available size', async () => {
+      const validSkus = [validSkuOutOfStockOnline, { ...validSkuOutOfStock, _id: 'skuId2' } ]
+      const result = buildSizesArray(validSkus);
+      expect(result).toEqual([ { en: 'sizeEnOutOfStockOnline', fr: 'sizeFrOutOfStockOnline' } ]); 
+    });
+});
+
+describe('buildStoreInventory', () => {
+    it('two available skus from same store should produce two available sizes for same store id', async () => {
+      const validSkus = [validSku, { ...validSku, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' } }]
+      const result = buildStoreInventory(validSkus);
+      expect(result).toEqual({
+        'storeId': [{ en: 'sizeEn', fr: 'sizeFr' }, { en: 'sizeEn2', fr: 'sizeFr2' }]
+      }); 
+    });
+    it('two available skus from different stores should produce two available sizes for different store ids', async () => {
+      const validSkus = [validSku, { ...validSku, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' }, ats: [{ storeId: 'storeId2', availableToSell: 1}] }]
+      const result = buildStoreInventory(validSkus);
+      expect(result).toEqual({
+        'storeId': [{ en: 'sizeEn', fr: 'sizeFr' }],
+        'storeId2': [{ en: 'sizeEn2', fr: 'sizeFr2' }]
+      }); 
+    });
+    it('two available skus from same stores with same sizes should produce one available size for one store id', async () => {
+      const validSkus = [validSku, { ...validSku, _id: 'skuId2' }]
+      const result = buildStoreInventory(validSkus);
+      expect(result).toEqual({
+        'storeId': [{ en: 'sizeEn', fr: 'sizeFr' }]
+      }); 
+    });
+    it('two available skus from different stores with same sizes should produce one available size for different store ids', async () => {
+      const validSkus = [validSku, { ...validSku, _id: 'skuId2', ats: [{ storeId: 'storeId2', availableToSell: 1}] }]
+      const result = buildStoreInventory(validSkus);
+      expect(result).toEqual({
+        'storeId': [{ en: 'sizeEn', fr: 'sizeFr' }],
+        'storeId2': [{ en: 'sizeEn', fr: 'sizeFr' }]
+      }); 
+    });
+
+    it('one available and one unavailable sku from same stores should produce one available size for one store id', async () => {
+      const validSkus = [validSkuOutOfStock, { ...validSku, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' } }]
+      const result = buildStoreInventory(validSkus);
+      expect(result).toEqual({
+        'storeId': [{ en: 'sizeEn2', fr: 'sizeFr2' }]
+      }); 
+    });
+    it('one available and one unavailable sku from different stores should produce one available size for different store ids', async () => {
+      const validSkus = [validSkuOutOfStock, { ...validSku, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' }, ats: [{ storeId: 'storeId2', availableToSell: 1}] }]
+      const result = buildStoreInventory(validSkus);
+      expect(result).toEqual({
+        'storeId2': [{ en: 'sizeEn2', fr: 'sizeFr2' }]
+      }); 
+    });
+    it('no available skus from different stores should produce no available sizes for different store ids', async () => {
+      const validSkus = [validSkuOutOfStock, { ...validSkuOutOfStock, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' } }]
+      const result = buildStoreInventory(validSkus);
+      expect(result).toEqual({}); 
+    });
+    it('one online available sku and one instore available sku from different stores should produce one available sizes for different store ids', async () => {
+      const validSkus = [validSkuOutOfStock, { ...validSkuOutOfStockOnline, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' }, ats: [{ storeId: 'storeId2', availableToSell: 1}] }]
+      const result = buildStoreInventory(validSkus);
+      expect(result).toEqual({
+        'storeId2': [{ en: 'sizeEn2', fr: 'sizeFr2' }]
+      }); 
+    });
+    it('one online available sku and one instore available sku from same stores should produce one available sizes for different store ids', async () => {
+      const validSkus = [validSkuOutOfStock, { ...validSkuOutOfStockOnline, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' }, ats: [{ storeId: 'storeId', availableToSell: 1}] }]
+      const result = buildStoreInventory(validSkus);
+      expect(result).toEqual({
+        'storeId': [{ en: 'sizeEn2', fr: 'sizeFr2' }]
+      }); 
+    });
+});
+
+describe('buildStoresArray', () => {
+    it('two available skus should produce two available stores', async () => {
+      const validSkus = [validSku, { ...validSku, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' }, ats: [{ storeId: 'storeId2', availableToSell: 1}] }]
+      const result = buildStoresArray(validSkus);
+      expect(result).toEqual([ 'storeId', 'storeId2' ]); 
+    });
+    it('two available skus with duplicate storeids should produce one available stores', async () => {
+      const validSkus = [validSku, { ...validSku, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' }, ats: [{ storeId: 'storeId', availableToSell: 1}] }]
+      const result = buildStoresArray(validSkus);
+      expect(result).toEqual([ 'storeId' ]); 
+    });
+    it('one available sku out of two should produce one available stores', async () => {
+      const validSkus = [validSkuOutOfStock, { ...validSku, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' }, ats: [{ storeId: 'storeId2', availableToSell: 1}] }]
+      const result = buildStoresArray(validSkus);
+      expect(result).toEqual([ 'storeId2' ]); 
+    });
+    it('no available sku out of two should produce no available stores', async () => {
+      const validSkus = [validSkuOutOfStock, { ...validSku, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' }, ats: [] }]
+      const result = buildStoresArray(validSkus);
+      expect(result).toEqual([]); 
+    });
+    it('one online available sku and one instore available sku should produce one available stores', async () => {
+      const validSkus = [validSkuOutOfStock, { ...validSkuOutOfStockOnline, _id: 'skuId2', size: { en: 'sizeEn2', fr: 'sizeFr2' }, ats: [{ storeId: 'storeId2', availableToSell: 1}] }]
+      const result = buildStoresArray(validSkus);
+      expect(result).toEqual([ 'storeId2' ]); 
     });
 });
