2943c184d947b85c1eaaa61d314a906d45bbeea1
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-09-29 08:57:04 -0400

    HRC-3534 Implement helpers for getting SKU ATS

--- ibmCloudFunctions/product-consumers/updateAlgoliaInventory/utils.js
+++ ibmCloudFunctions/product-consumers/updateAlgoliaInventory/utils.js
@@ -1,3 +1,6 @@
+const { productApiRequest } = require('../../lib/productApi');
+const { addErrorHandling } = require('../utils');
+
 const getTotalAts = (sku, atsKey) => (sku[atsKey] || []).reduce((totalAvailableToSell, { availableToSell }) => (
       availableToSell > 0 ? totalAvailableToSell + availableToSell : totalAvailableToSell
     ), 0)
@@ -55,8 +58,27 @@ const buildStoresArray = (
       return stores;
      }
 
+
+const getSkuAtsByStyleAndSkuId = (styleId, params) => async skuId => (
+  {
+    styleId,
+    skuId,
+    ...await productApiRequest(params, `/inventory/ats/${styleId}/${skuId}`)
+  }
+)
+
+const getSkuInventoryFromStyleIdsBatchedByStyleId = ({ styleIds, skuCollection, params }) => (
+  Promise.all(styleIds.map(addErrorHandling(async styleId => {
+    const skus = await skuCollection.find({ styleId }).toArray()
+    const skuIds = skus.map(sku => sku._id)
+    return Promise.all(skuIds.map(getSkuAtsByStyleAndSkuId(styleId, params)))
+  })))
+)
+
 module.exports = {
   buildSizesArray,
   buildStoreInventory,
   buildStoresArray,
+  getSkuAtsByStyleAndSkuId,
+  getSkuInventoryFromStyleIdsBatchedByStyleId
 };
