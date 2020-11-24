40358d5472c62e9ba2e3d0d2e962e4bd0169ec7e
Houman Kamali <houmam.kamali@outlook.com>
2020-11-24 17:54:44 -0500

    HRC-3772: add DLQ query builder

---
+++ tools/apps/kafkaMonitoring/frontend/src/DlqQueryBuilder.js
@@ -0,0 +1,72 @@
+import { Input, DatePicker, Select } from 'antd';
+import { useState } from 'react';
+import { useCopyToClipboard } from 'react-use';
+
+export default function DlqQueryBuilder () {
+  const [cloudFunctionName, setCloudFunctionName] = useState('')
+  const [messages, setMessages] = useState([[]])
+  const [date, setDate] = useState(null)
+  const [dateSelector, setDateSelector] = useState('after')
+
+  const [{value, error, noUserInteraction}, copyToClipboard] = useCopyToClipboard()
+
+  const searchByCloudFunctionNameQuery = cloudFunctionName
+    ? `"metadata.activationInfo.name": "${cloudFunctionName}"`
+    : ''
+  const searchByKeyValueQuery = messages[0][0] && messages[0][1]
+    ? `"messages.value.${messages[0][0]}": "${messages[0][1]}"`
+    : ''
+  const searchByActivationStartDate = date
+    ? `"metadata.activationInfo.start": { ${dateSelector === 'after' ? '"$gt"' : "$lt" }: ${date} }`
+    : ''
+  const query = `{ ${[
+    searchByCloudFunctionNameQuery,
+    searchByKeyValueQuery,
+    searchByActivationStartDate
+  ].filter(Boolean).join(', ')} }`
+  return (
+    <>
+      <div>
+        <label>Query by Cloud Function Name e.g. <strong>consume-threshold-message</strong>:</label>
+        <Input
+          placeholder="cloud function name"
+          value={cloudFunctionName}
+          onChange={e => setCloudFunctionName(e.target.value)}
+        />
+      </div>
+      <br />
+      <div>
+        <label>Query by Message key: value pair e.g. <strong>STYLEID: "100"</strong>:</label>
+        <Input
+          placeholder={`field`}
+          value={messages[0][0]}
+          onChange={e => setMessages(prev => [[e.target.value, prev[0][1]]])}
+        />
+        <Input
+          placeholder={`value`}
+          value={messages[0][1]}
+          onChange={e => setMessages(prev => [[prev[0][0], e.target.value]])}
+        />
+        <p>Note: If the value is not string e.g. is a boolean or a number, you have to manually set it in the query</p>
+      </div>
+      <br />
+      <div>
+        <label>Query by Activation Start Date</label>
+        <div>
+          <Select style={{ width: 120 }} defaultValue={dateSelector} onChange={setDateSelector}>
+            <Select.Option value="before">before</Select.Option>
+            <Select.Option value="after">after</Select.Option>
+          </Select>
+          <DatePicker showTime onChange={setDate} onOk={date => setDate(date.unix())} />
+        </div>
+      </div>
+      <br />
+      <div>
+        <label htmlFor="filter-query">Filter </label><button disabled={!query} onClick={() => copyToClipboard(query)}>copy</button>
+        <p id="filter-query">
+          <strong>{query}</strong>
+        </p>
+      </div>
+    </>
+  )
+}
