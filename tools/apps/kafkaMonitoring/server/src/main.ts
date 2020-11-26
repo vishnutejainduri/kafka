1040ced283fb4ff03b80e9df4618afb76d328122
Houman Kamali <houmam.kamali@outlook.com>
2020-11-26 06:20:04 -0500

    HRC-3772: set default port to 8080

--- tools/apps/kafkaMonitoring/server/src/main.ts
+++ tools/apps/kafkaMonitoring/server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 8080;
   await app.listen(port);
   console.log('Listing on port ', port);
 }
