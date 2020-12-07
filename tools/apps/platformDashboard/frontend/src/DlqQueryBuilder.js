752d45567faf672d2d6fb1758949c6ef9da1cd01
Houman Kamali <houmam.kamali@outlook.com>
2020-12-07 14:27:11 -0500

    HRC-3772: add more options to dlq builder

--- tools/apps/platformDashboard/frontend/src/DlqQueryBuilder.js
+++ tools/apps/platformDashboard/frontend/src/DlqQueryBuilder.js
@@ -3,15 +3,23 @@ import { useState } from 'react';
 import { useCopyToClipboard } from 'react-use';
 
 export default function DlqQueryBuilder () {
+  const [activationId, setActivationId] = useState('')
   const [cloudFunctionName, setCloudFunctionName] = useState('')
+  const [topicName, setTopicName] = useState('')
   const [messages, setMessages] = useState([[]])
   const [date, setDate] = useState(null)
   const [dateSelector, setDateSelector] = useState('after')
 
   const [, copyToClipboard] = useCopyToClipboard()
 
+  const searchByTopicName = topicName
+    ? `"messages.topic": "${topicName}"`
+    : ''
   const searchByCloudFunctionNameQuery = cloudFunctionName
     ? `"metadata.activationInfo.name": "${cloudFunctionName}"`
+    : ''
+  const searchByActivationId = activationId
+    ? `"metadata.activationInfo.activationId": "${activationId}"`
     : ''
   const searchByKeyValueQuery = messages[0][0] && messages[0][1]
     ? `"messages.value.${messages[0][0]}": "${messages[0][1]}"`
@@ -19,16 +27,28 @@ export default function DlqQueryBuilder () {
   const searchByActivationStartDate = date
     ? `"metadata.activationInfo.start": { ${dateSelector === 'after' ? '"$gt"' : "$lt" }: ${date * 1000} }`
     : ''
+  
   const query = `{ ${[
+    searchByTopicName,
     searchByCloudFunctionNameQuery,
     searchByKeyValueQuery,
-    searchByActivationStartDate
+    searchByActivationStartDate,
+    searchByActivationId
   ].filter(Boolean).join(', ')} }`
 
   return (
     <>
       <div>
-        <label>Query by Cloud Function Name e.g. consume-threshold-message:</label>
+        <label>Query by <strong>ActivationId</strong> e.g. b8eeabe6-128f-4d30-84bf-defa9a4ea748:</label>
+        <Input
+          placeholder="activation ID"
+          value={activationId}
+          onChange={e => setActivationId(e.target.value)}
+        />
+      </div>
+      <br />
+      <div>
+        <label>Query by <strong>Cloud Function Name</strong> e.g. consume-threshold-message:</label>
         <Input
           placeholder="cloud function name"
           value={cloudFunctionName}
@@ -37,7 +57,16 @@ export default function DlqQueryBuilder () {
       </div>
       <br />
       <div>
-        <label>Query by Message key: value pair e.g. STYLEID: "100":</label>
+        <label>Query by <strong>Topic Name</strong> e.g. consume-sale-price-ct:</label>
+        <Input
+          placeholder="topic name"
+          value={topicName}
+          onChange={e => setTopicName(e.target.value)}
+        />
+      </div>
+      <br />
+      <div>
+        <label>Query by <strong>Message key: value</strong> pair e.g. STYLEID: "100":</label>
         <Input
           placeholder={`field`}
           value={messages[0][0]}
@@ -50,9 +79,8 @@ export default function DlqQueryBuilder () {
         />
         <p>Note: If the value is not string e.g. is a boolean or a number, you have to manually set it in the query</p>
       </div>
-      <br />
       <div>
-        <label>Query by Activation Start Date (Browser Local Time)</label>
+        <label>Query by <strong>Activation Start Date</strong> (Browser Local Time)</label>
         <div>
           <Select style={{ width: 120 }} defaultValue={dateSelector} onChange={setDateSelector}>
             <Select.Option value="before">before</Select.Option>
