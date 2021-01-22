ddf11e5aa14a67b394ac5911eac2cae207821c34
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2021-01-22 09:13:53 -0500

    HRC-4017 Process English and French CSVs in separate runs of the CF

--- ibmCloudFunctions/commercetools/exportCatalog/index.js
+++ ibmCloudFunctions/commercetools/exportCatalog/index.js
@@ -1,4 +1,3 @@
-const { languageKeys } = require('../constantsCt')
 const { getFormatStream } = require('./csv')
 const { getFtpClient, pipeStreamToServer } = require('./ftp')
 const { outputFilenames } = require('./config')
@@ -7,37 +6,19 @@ const { getProductExporter } = require('./commercetools')
 const main = async params => {
   const ftpClient = await getFtpClient(params)
   const productExporter = await getProductExporter(params)
-  const errors = []
+  const locale = params.locale
+  const stream = getFormatStream(locale, params)
+  const outputFilename = outputFilenames[locale]
   
-  const englishConfig = {
-    stream: getFormatStream(languageKeys.ENGLISH, params),
-    outputFilename: outputFilenames[languageKeys.ENGLISH]
-  }
-
-  const frenchConfig = {
-    stream: getFormatStream(languageKeys.FRENCH, params),
-    outputFilename: outputFilenames[languageKeys.FRENCH]
-  }
-
-  // Must be sequential because FTP doesn't support concurrent uploads from one connection
-  for (const { stream, outputFilename } of ([englishConfig, frenchConfig])) {
-    productExporter.run(stream)
-    console.log(`Starting to process ${outputFilename}`)
-    try {
-      await pipeStreamToServer({ ftpClient, readStream: stream, outputFilename })
-      console.log(`Successfully processed ${outputFilename}`)
-    } catch (error) {
-      console.error(`Unable to upload ${outputFilename}:`, error.message)
-      errors.push(error.message)
-    }
-  }
-
-  ftpClient.end()
-
-  if (errors.length === 0) {
-    console.log('Uploaded all product catalogs successfully successfully')
-  } else {
-    throw new Error(`Errors while trying to process product catalog (${errors.length} total):`, JSON.stringify(errors))
+  productExporter.run(stream)
+  console.log(`Starting to process ${outputFilename} (${locale})`)
+  try {
+    await pipeStreamToServer({ ftpClient, readStream: stream, outputFilename })
+    console.log(`Successfully uploaded ${outputFilename}`)
+  } catch (error) {
+    console.error(`Unable to upload ${outputFilename}:`, error.message)
+  } finally {
+    ftpClient.end()
   }
 }
 
