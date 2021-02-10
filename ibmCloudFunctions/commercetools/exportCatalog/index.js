e5bc48cb5dd78949d6536ff08c90307491cb2d3f
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2021-02-10 09:20:08 -0500

    HRC-4097 Replace catalog export CF with one that calls product API to trigger export

--- ibmCloudFunctions/commercetools/exportCatalog/index.js
+++ ibmCloudFunctions/commercetools/exportCatalog/index.js
@@ -1,41 +1,20 @@
-const path = require('path')
-const SftpClient = require('ssh2-sftp-client')
+const fetch = require('node-fetch').default
 const { logAndThrowErrorMessage } = require('../../product-consumers/utils')
-const { getProductExporter } = require('./commercetools')
-const { outputFilenames } = require('./config')
-const { getFormatStream } = require('./csv')
-
-const sftpClient = new SftpClient()
 
 const main = async params => {
-  const productExporter = await getProductExporter(params)
-  const locale = params.locale
-  const stream = getFormatStream(locale, params)
-  const outputFilename = outputFilenames[locale]
-  const outputPath = path.join(params.hrSftpFolder, outputFilename)
+  const { ctpApiExtensionsAuthorization, productExportTriggerUrl } = params
 
   try {
-    await sftpClient.connect({
-      host: params.hrSftpHost,
-      port: params.hrSftpPort,
-      username: params.hrSftpUser,
-      password: params.hrSftpPassword
+    const response = await fetch(productExportTriggerUrl, {
+      method: 'post',
+      headers: { authorization: ctpApiExtensionsAuthorization }
     })
-    console.log('Connected to SFTP server')
+    if (!(response.status === 202)) throw new Error(`Received non-202 response from product API: ${response.status} ${await response.text()}`)
   } catch (error) {
-    logAndThrowErrorMessage(`Unable to connect to SFTP server: ${error.message}`)
+    logAndThrowErrorMessage(error.message)
   }
 
-  productExporter.run(stream)
-  console.log(`Starting to process ${outputPath} (${locale})`)
-  try {
-    await sftpClient.put(stream, outputPath)
-    console.log(`Successfully uploaded ${outputPath}`)
-  } catch (error) {
-    logAndThrowErrorMessage(`Unable to upload ${outputPath}: ${error.message}`)
-  } finally {
-    await sftpClient.end()
-  }
+  console.log('Successfully triggered catalog export')
 }
 
 global.main = main
