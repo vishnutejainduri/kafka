7a47abfc5ab3caa527ee78f088e09e50910efe65
Shreyansh Kumar <shreyansh.k@myplanet.com>
2021-02-19 14:28:59 -0500

    srew up with re-sync

--- ibmCloudFunctions/commercetools/consumeCatalogMessageCT/__tests__/consumeCatalogCTMessage.test.js
+++ ibmCloudFunctions/commercetools/consumeCatalogMessageCT/__tests__/consumeCatalogCTMessage.test.js
@@ -265,21 +265,6 @@ const styleActions = [
   },
   {
     action: 'setAttributeInAllVariants',
-    name: 'relatedProductId',
-    staged: false,
-    value: 'vsn341brandNameEng',
-  },
-  {
-    action: 'setAttributeInAllVariants',
-    name: 'promotionalSticker',
-    staged: false,
-    value: {
-      "en-CA": "Final Sale",
-      "fr-CA": "Final Sale"
-    }
-  },
-  {
-    action: 'setAttributeInAllVariants',
     name: 'styleLastModifiedInternal',
     staged: false,
     value: new Date('2016-08-05T10:03:59.002Z'),
