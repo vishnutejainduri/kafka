d4ecdf31dbea019d652d13162b8f5c949ea4a61b
Shreyansh Kumar <shreyansh.k@myplanet.com>
2021-02-19 14:20:58 -0500

    reverted back to 'Approved' instead

--- ibmCloudFunctions/product-consumers/consumeCatalogMessage/__tests__/consumeCatalogMessage.test.js
+++ ibmCloudFunctions/product-consumers/consumeCatalogMessage/__tests__/consumeCatalogMessage.test.js
@@ -168,7 +168,7 @@ describe('parseStyleMessage', () => {
     });
 
     it('maps webstatus to true if it is approved', () => {
-        const messageWithTrueWebStatus = { ...testData, value: { ...testData.value, WEBSTATUS: 'Y' } }
+        const messageWithTrueWebStatus = { ...testData, value: { ...testData.value, WEBSTATUS: 'Approved' } }
         expect(parseStyleMessage(messageWithTrueWebStatus).webStatus).toBe(true)
     });
 });
