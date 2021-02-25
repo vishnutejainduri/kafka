c60a1f1a75090e5e0a6e7ad4ec0775caf72e5814
Shreyansh Kumar <shreyansh.k@myplanet.com>
2021-02-25 16:24:26 -0500

    fixed promo sticker value name

--- ibmCloudFunctions/product-consumers/addFacetsToBulkImportQueue/utils.js
+++ ibmCloudFunctions/product-consumers/addFacetsToBulkImportQueue/utils.js
@@ -1,11 +1,11 @@
 const { PROMO_STICKER } = require('../../lib/constants');
 
-const messageIncludesStickerForNonReturnableStyle = styles => async (facetData) => {
+const removeNotReturnablePromoStickerMessage = styles => async (facetData) => {
   const styleData = await styles.findOne({ _id: facetData.styleId })
 
   return facetData.facetName !== PROMO_STICKER || styleData.isReturnable === true
 };
 
 module.exports = {
-  messageIncludesStickerForNonReturnableStyle
+  removeNotReturnablePromoStickerMessage
 }
